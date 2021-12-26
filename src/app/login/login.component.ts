00c08d0063fab859577930de1c14d235668c5521
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-12-26 17:46:09 -0300

    Added login with google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { 
+  }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    const provider = (new firebase.auth.GoogleAuthProvider());
+    return this.afAuth.auth.signInWithRedirect(provider).then(value => {
+      console.log('Sucess', value)
+    })
+    .catch(error => {
+      console.log('Something went wrong: ', error);
+    });
   }
 
 }
