145a1e5146aaa0c1e09caefc3c3e63dd700d4bb7
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-12-26 07:32:06 -0300

    Added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC5VxS5CfKk5kJowPA9OH-spkU5r-rL708",
+    authDomain: "electronicshop-582a9.firebaseapp.com",
+    projectId: "electronicshop-582a9",
+    storageBucket: "electronicshop-582a9.appspot.com",
+    messagingSenderId: "143645838772",
+    appId: "1:143645838772:web:935ddf50f3aadbc429b6e1",
+    measurementId: "G-REJQENLG8E"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC5VxS5CfKk5kJowPA9OH-spkU5r-rL708",
+    authDomain: "electronicshop-582a9.firebaseapp.com",
+    projectId: "electronicshop-582a9",
+    storageBucket: "electronicshop-582a9.appspot.com",
+    messagingSenderId: "143645838772",
+    appId: "1:143645838772:web:935ddf50f3aadbc429b6e1",
+    measurementId: "G-REJQENLG8E"
+  }
 };
 
 /*
