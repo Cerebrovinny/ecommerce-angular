55054ce525a88caee8fa70064e87a2349e997c1f
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-12-31 15:44:27 -0300

    save users in database

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database'
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,9 @@ import { AngularFireDatabase } from '@angular/fire/database'
 export class UserService {
 user!: firebase.User
 
-
   constructor(private db: AngularFireDatabase) { }
 
-    save(user: any){
+    save(user: firebase.User){
       this.db.object('/users/' + this.user.uid).update({
         name: user.displayName,
         email: user.email
